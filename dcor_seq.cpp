25f6efc7a1e578f668fa5838d9716bc7ec4075ba
Benjamin Guillet <benjamin.guillet@gmail.com>
2013-04-26 17:01:16 -0500

    fix algorithm

--- dcor_seq.cpp
+++ dcor_seq.cpp
@@ -49,25 +49,25 @@ void get_least_loss(std::vector< std::string > params, std::vector< std::vector<
     int best_start_date = 0;
     int best_end_date   = 0;
     float best_return = -std::numeric_limits<float>::max();
-    float current_return = 0.0;
-    float value_start_date = 0.0;
-    float value_current_date = 0.0;
+    float value_asset1_start_date = 0.0; 
+    float value_asset2_start_date = 0.0;
+    float return_asset1 = 0.0;
+    float return_asset2 = 0.0;
+    float total_current_return = 0.0;
     
     int n_rows = data[0].size();
     //std::cout << n_rows << std::endl;
-    // optimization: right now some return of rows are computed multiple times,
-    // return should be computed once and stored and reused
     for (int index_start_date = 0; index_start_date < n_rows - 1; ++index_start_date) {
-        value_start_date =  perc1 * ::atof(data[1][index_start_date].c_str()) + perc2 * ::atof(data[2][index_start_date].c_str());
-        //std::cout << value_start_date << std::endl;
+        value_asset1_start_date = ::atof(data[1][index_start_date].c_str());
+        value_asset2_start_date = ::atof(data[2][index_start_date].c_str());
         for (int current_date = index_start_date + 1; current_date < n_rows; ++current_date) {
-            value_current_date =  perc1 * ::atof(data[1][current_date].c_str()) + perc2 * ::atof(data[2][current_date].c_str());
-            //std::cout << value_current_date << std::endl;
-            current_return = value_current_date - value_start_date;
-            //std::cout << current_return << std::endl; 
-            if (current_return > best_return) {
-                std::cout << "we have a better return: " << current_return << std::endl;
-                best_return = current_return;
+            return_asset1 = (::atof(data[1][current_date].c_str()) - value_asset1_start_date) / value_asset1_start_date;
+            return_asset2 = (::atof(data[2][current_date].c_str()) - value_asset2_start_date) / value_asset2_start_date;
+            total_current_return =  (perc1 * return_asset1 + perc2 * return_asset2) * 100.0;
+            
+            if (total_current_return > best_return) {
+                std::cout << "we have a better return: " << total_current_return << std::endl;
+                best_return = total_current_return;
                 best_start_date = index_start_date;
                 best_end_date = current_date;
             }
@@ -76,6 +76,42 @@ void get_least_loss(std::vector< std::string > params, std::vector< std::vector<
     std::cout << "Best return is " << best_return;
     std::cout << " if you start on " << data[0][best_start_date];
     std::cout << " and stop on " << data[0][best_end_date];
+    std::cout << std::endl;
+}
+
+void get_greatest_loss(std::vector< std::string > params, std::vector< std::vector< std::string > > data) {
+    float perc1 = ::atof(params[5].c_str()) / 100.0;
+    float perc2 = ::atof(params[3].c_str()) / 100.0;
+    int worst_start_date = 0;
+    int worst_end_date   = 0;
+    float worst_return = std::numeric_limits<float>::max();
+    float value_asset1_start_date = 0.0; 
+    float value_asset2_start_date = 0.0;
+    float return_asset1 = 0.0;
+    float return_asset2 = 0.0;
+    float total_current_return = 0.0;
+    
+    int n_rows = data[0].size();
+    //std::cout << n_rows << std::endl;
+    for (int index_start_date = 0; index_start_date < n_rows - 1; ++index_start_date) {
+        value_asset1_start_date = ::atof(data[1][index_start_date].c_str());
+        value_asset2_start_date = ::atof(data[2][index_start_date].c_str());
+        for (int current_date = index_start_date + 1; current_date < n_rows; ++current_date) {
+            return_asset1 = (::atof(data[1][current_date].c_str()) - value_asset1_start_date) / value_asset1_start_date;
+            return_asset2 = (::atof(data[2][current_date].c_str()) - value_asset2_start_date) / value_asset2_start_date;
+            total_current_return =  (perc1 * return_asset1 + perc2 * return_asset2) * 100.0;
+            
+            if (total_current_return < worst_return) {
+                std::cout << "we have a worst return: " << total_current_return << std::endl;
+                worst_return = total_current_return;
+                worst_start_date = index_start_date;
+                worst_end_date = current_date;
+            }
+        }
+    }
+    std::cout << "Worst return is " << worst_return;
+    std::cout << " if you start on " << data[0][worst_start_date];
+    std::cout << " and stop on " << data[0][worst_end_date];
     std::cout << std::endl;
 }
 
@@ -96,6 +132,7 @@ int main(int argc, const char *argv[]) {
     data = fetch_data(conn, params);
     print_data(data);
     get_least_loss(params, data);
+    get_greatest_loss(params, data);
     
     return 0;
 }
